9d2ed1271ba2edfefe716c7d7fbd93c3617fc55f
SHALOM P <68636571+Shalom-P@users.noreply.github.com>
2020-08-08 14:59:51 +0530

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -24,24 +24,5 @@ pollutantmean<- function(directory,pollutant,id ){
   }
   mean(alln)
 }
-complete<-function(directory,id){
-  t<-numeric(length(id))
-  t<-NULL
-  for(i in  id){
-    if(i<10){
-      a<-paste(paste('00',i,sep = ""),".csv",sep = "")
-    }
-    else if(i<100){
-      a<-paste(paste('0',i,sep = ""),".csv",sep = "")
-    }
-    else{
-      a<-paste(i,".csv",sep = "")
-    }
-    b<-read.csv(a)
-    t<-c(t,length(b$ID[complete.cases(b)]))
-  }
-  data.frame("id"=id,"nobs"=t)
-  
-}
 
     
