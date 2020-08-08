9cd0fe216be7fb95800ccaf06fd34e1df9b7f405
Shalom P <rahulnalluru007@gmail.com>
2020-08-08 11:25:41 +0530

    Initial commit

---
+++ pollutantmean.R
@@ -0,0 +1,29 @@
+pollutantmean<- function(directory,pollutant,id ){
+  alln=numeric()
+  for(i in id){
+    t<-numeric(400)
+    if(i<10){
+      a<-paste(paste('00',i,sep = ""),".csv",sep = "")
+    }
+    else if(i<100){
+      a<-paste(paste('0',i,sep = ""),".csv",sep = "")
+    }
+    else{
+      a<-paste(i,".csv",sep = "")
+    }
+    b<-read.csv(a)
+    if(identical(pollutant,"sulfate")){
+      C<-b$sulfate[complete.cases(b$sulfate)]
+      #print(c)
+    }
+    else if(identical(pollutant,"nitrate")){
+      C<-b$nitrate[complete.cases(b$nitrate)]
+      #print(c)
+    }
+    alln<-c(alln,C)
+  }
+  mean(alln)
+}
+complete<-function(directory,id)
+
+    
